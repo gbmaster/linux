9ae4d92954f13a463f120431bbf6b9ad029e714c
Aldo Mazzeo <aldo.mazzeo@gmail.com>
2002-03-29 22:47:33 +0000

    Import 2.4.19-pre5

--- drivers/pcmcia/sa1100_generic.h
+++ drivers/pcmcia/sa1100_generic.h
@@ -6,10 +6,8 @@
  * This file contains definitions for the low-level SA-1100 kernel PCMCIA
  * interface. Please see linux/Documentation/arm/SA1100/PCMCIA for details.
  */
-
 #ifndef _ASM_ARCH_PCMCIA
 #define _ASM_ARCH_PCMCIA
-
 
 /* Ideally, we'd support up to MAX_SOCK sockets, but the SA-1100 only
  * has support for two. This shows up in lots of hardwired ways, such
@@ -18,9 +16,6 @@
  * in this way doesn't seem too terrible.
  */
 #define SA1100_PCMCIA_MAX_SOCK   (2)
-
-
-#ifndef __ASSEMBLY__
 
 struct pcmcia_init {
   void (*handler)(int irq, void *dev, struct pt_regs *regs);
@@ -47,7 +42,8 @@ struct pcmcia_configure {
             vpp: 8,
          output: 1,
         speaker: 1,
-          reset: 1;
+          reset: 1,
+            irq: 1;
 };
 
 struct pcmcia_irq_info {
@@ -61,10 +57,20 @@ struct pcmcia_low_level {
   int (*socket_state)(struct pcmcia_state_array *);
   int (*get_irq_info)(struct pcmcia_irq_info *);
   int (*configure_socket)(const struct pcmcia_configure *);
+
+  /*
+   * Enable card status IRQs on (re-)initialisation.  This can
+   * be called at initialisation, power management event, or
+   * pcmcia event.
+   */
+  int (*socket_init)(int sock);
+
+  /*
+   * Disable card status IRQs and PCMCIA bus on suspend.
+   */
+  int (*socket_suspend)(int sock);
 };
 
 extern struct pcmcia_low_level *pcmcia_low_level;
-
-#endif  /* __ASSEMBLY__ */
 
 #endif
